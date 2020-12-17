21e810aacbd26e6853fb19a1afc6bdc60c7f83dc
Jaime G. Llistó <30316290+jaimegonzall@users.noreply.github.com>
2020-12-17 11:21:44 +0100

    Routes login to express

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+'use strict'
+
 const express = require('express');
+
+const router = require('./routes/login.routes.js')
 const configuration = require('./configuration/develop.json');
 
 const app = express();
 const port = configuration.server.port;
+
+app.use(router);
 
 app.listen(port, () => {
     console.log(`${port} express escuchando`);
