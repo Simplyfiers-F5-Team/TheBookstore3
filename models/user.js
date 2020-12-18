f63cbe5e133276cecfa0c46b308e4dd9c677335b
Jaime G. Llistó <30316290+jaimegonzall@users.noreply.github.com>
2020-12-18 12:56:14 +0100

    Scheme for user and book

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require(`mongoose`);
+/* import { Schema, model } from `mongoose`;  JS*/
+/* se crea el esquma de usuario llamado UserSchema y se la pasan los parámetros del type unico (para que no se repitan, que no haya dos usuarios con el mismo nombre) */
+/* require : true para que sea obligatorio escribir el nombre de usuario */
+/* timestamp añade el día de hora de creación del usuario y la fecha de la última actualización */
+
+const UserSchema = new Schema({
+    userName: {
+        type: String,
+        unique: true,
+        require: true
+    },
+    timestamps: true
+});
+
+module.exports = model('User', UserSchema);
