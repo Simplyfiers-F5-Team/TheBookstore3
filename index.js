1d9562a06948a61e54925f8e77285733859c2951
Jaime G. Llistó <30316290+jaimegonzall@users.noreply.github.com>
2021-01-08 13:08:41 +0100

    Revert "CRUD"
    This reverts commit dc0e1edf862de6e2823c39c257bd908052514f9d.

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const configuration = require('./configuration/develop.json');
 mongoose.connect("mongodb://localhost:27017/TheBookstore", {
     useUnifiedTopology: true, 
     useNewUrlParser: true,
-    useCreateIndex: true
 });
 
 //especificamos el mensaje que queremos que salga cuando se conecte
@@ -25,7 +24,7 @@ const port = configuration.server.port;
 //le decimos a express q a parte de usar las rutas, le vamos a dar json y datos en la url
 //express.json y urlencoded tiene que ir antes de las rutas
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded());
 app.use(router);
 
 app.engine('hbs', exphbs({
